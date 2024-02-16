84b39adba1dc39059893621a160057d6fe5ab1e8
s11leo <104096191+s11leo@users.noreply.github.com>
2024-02-16 13:54:51 +0200

    Update app.js

--- app_page/app.js
+++ app_page/app.js
@@ -3,7 +3,6 @@ document.querySelector('#wallet-connect .button').addEventListener('click', asyn
         try {
             const response = await window.solana.connect({ onlyIfTrusted: false });
             console.log('Connected with Public Key:', response.publicKey.toString());
-            // Форматирование и отображение адреса кошелька на кнопке
             const address = response.publicKey.toString();
             const formattedAddress = `${address.slice(0, 4)}...${address.slice(-4)}`;
             document.querySelector('#wallet-connect .button').textContent = formattedAddress;
