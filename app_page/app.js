c35651a8887e88e0a52058812e9f8e51f3f9009f
root <sychovnik@mail.com>
2024-03-12 09:24:18 +0000

    updates

--- app_page/app.js
+++ app_page/app.js
@@ -9,6 +9,16 @@ document.querySelector('#wallet-connect .button').addEventListener('click', asyn
             
             localStorage.setItem('walletAddress', address);
             // await getTokensBalance(new solanaWeb3.PublicKey(address));
+            const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('devnet'), 'confirmed');
+
+            // check balance SOL
+            const solBalance = await connection.getBalance(walletAddress);
+            // console.log('Sol balance', solBalance);
+            const solBalanceInLamports = solBalance;
+            const solBalanceInSOL = solBalanceInLamports / solanaWeb3.LAMPORTS_PER_SOL;
+            console.log('Sol balance', solBalanceInSOL);
+
+            localStorage.setItem('solBalanceInSOL', solBalanceInSOL);
             
         } catch (err) {
             console.error('Error connecting to Phantom wallet:', err);
@@ -18,53 +28,53 @@ document.querySelector('#wallet-connect .button').addEventListener('click', asyn
     }
 });
 
-document.querySelector('#my-assets-connect').addEventListener('click', async () => {
-    try {
-        const address = localStorage.getItem('walletAddress');
-        console.log('address = localStorage', address);
-        if(address) {
-            await getTokensBalance(new solanaWeb3.PublicKey(address));
-        }
-    } catch (err) {
-        console.error('Error connecting to Phantom wallet:', err);
-    }
-});
+// document.querySelector('#my-assets-connect').addEventListener('click', async () => {
+//     try {
+//         const address = localStorage.getItem('walletAddress');
+//         console.log('address = localStorage', address);
+//         if(address) {
+//             await getTokensBalance(new solanaWeb3.PublicKey(address));
+//         }
+//     } catch (err) {
+//         console.error('Error connecting to Phantom wallet:', err);
+//     }
+// });
 
-async function getTokensBalance(walletAddress) {
+// async function getTokensBalance(walletAddress) {
     
-    const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('devnet'), 'confirmed');
+//     const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('devnet'), 'confirmed');
 
-    // check balance SOL
-    const solBalance = await connection.getBalance(walletAddress);
-    console.log('Sol balance', solBalance);
-    const solBalanceInLamports = solBalance;
-    const solBalanceInSOL = solBalanceInLamports / solanaWeb3.LAMPORTS_PER_SOL;
+//     // check balance SOL
+//     const solBalance = await connection.getBalance(walletAddress);
+//     console.log('Sol balance', solBalance);
+//     const solBalanceInLamports = solBalance;
+//     const solBalanceInSOL = solBalanceInLamports / solanaWeb3.LAMPORTS_PER_SOL;
 
-    // prepare token list
-    const tokensListElement = document.getElementById('tokens-list');
-    tokensListElement.innerHTML = '';
+//     // prepare token list
+//     const tokensListElement = document.getElementById('tokens-list');
+//     tokensListElement.innerHTML = '';
 
-    // adding information about the SOL balance
-    const solBalanceElement = document.createElement('div');
-    solBalanceElement.textContent = `SOL Balance: ${solBalanceInSOL.toFixed(2)} SOL`;
-    tokensListElement.appendChild(solBalanceElement);
+//     // adding information about the SOL balance
+//     const solBalanceElement = document.createElement('div');
+//     solBalanceElement.textContent = `SOL Balance: ${solBalanceInSOL.toFixed(2)} SOL`;
+//     tokensListElement.appendChild(solBalanceElement);
 
-    // get all token accounts by owner
-    const tokenAccounts = await connection.getParsedTokenAccountsByOwner(walletAddress, {
-        programId: new solanaWeb3.PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA')
-    });
+//     // get all token accounts by owner
+//     const tokenAccounts = await connection.getParsedTokenAccountsByOwner(walletAddress, {
+//         programId: new solanaWeb3.PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA')
+//     });
 
-    // displaying information for each token on the page
-    tokenAccounts.value.forEach(({ account }) => {
-        const tokenAmount = account.data.parsed.info.tokenAmount;
-        const tokenInfo = document.createElement('div');
-        tokenInfo.textContent = `Token: ${account.data.parsed.info.mint}, Balance: ${tokenAmount.uiAmount}`;
-        tokensListElement.appendChild(tokenInfo);
-    });
+//     // displaying information for each token on the page
+//     tokenAccounts.value.forEach(({ account }) => {
+//         const tokenAmount = account.data.parsed.info.tokenAmount;
+//         const tokenInfo = document.createElement('div');
+//         tokenInfo.textContent = `Token: ${account.data.parsed.info.mint}, Balance: ${tokenAmount.uiAmount}`;
+//         tokensListElement.appendChild(tokenInfo);
+//     });
 
-    if (tokenAccounts.value.length === 0) {
-        const noTokensElement = document.createElement('div');
-        noTokensElement.textContent = 'No tokens found.';
-        tokensListElement.appendChild(noTokensElement);
-    }
-}
+//     if (tokenAccounts.value.length === 0) {
+//         const noTokensElement = document.createElement('div');
+//         noTokensElement.textContent = 'No tokens found.';
+//         tokensListElement.appendChild(noTokensElement);
+//     }
+// }
