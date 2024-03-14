9bba231d1e4094dbd33f3bffa854387c0f501d1a
root <sychovnik@mail.com>
2024-03-14 19:04:27 +0000

    check balances updates

--- app_page/app.js
+++ app_page/app.js
@@ -31,7 +31,7 @@ window.addEventListener('modalOpened', async (e) => {
             console.error('Error connecting to Phantom wallet:', err);
         }
     }
-});
+
 
 async function getTokensBalance(walletAddress) {
     
@@ -71,3 +71,5 @@ async function getTokensBalance(walletAddress) {
         tokensListElement.appendChild(noTokensElement);
     }
 }
+
+});
