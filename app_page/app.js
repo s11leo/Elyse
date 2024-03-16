5bd5e09d2d007585a91067e9b238436836ebe392
root <sychovnik@mail.com>
2024-03-16 20:49:13 +0000

    view tokens on page - updates

--- app_page/app.js
+++ app_page/app.js
@@ -82,7 +82,7 @@ async function getWalletInfo() {
         console.log('SOL Balance:', solBalanceInSOL);
 
         const tokenBalances = await getTokensBalance(publicKey);
-        console.log('Token Balances:', SPLTokens);
+        console.log('Token Balances:', splTokenBalances);
 
         const walletEvent = new CustomEvent('walletInfo', {
             detail: {
